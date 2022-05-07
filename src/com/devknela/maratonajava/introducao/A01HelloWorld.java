d417dab5a67c3996e2ead019ce2a24c57500e43a
Lucas <lcseferreira@hotmail.com>
2022-05-07 09:28:15 -0300

    Tipos Primitivos

--- src/com/devknela/maratonajava/introducao/A01HelloWorld.java
+++ src/com/devknela/maratonajava/introducao/A01HelloWorld.java
@@ -4,12 +4,11 @@ package com.devknela.maratonajava.introducao;
  * Isso é um comentário <strong>JavaDoc</strong>
  */
 public class A01HelloWorld {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // Isso é um comentário
         System.out.println("Hello, World!");
         /*
         Comentário de múltiplas linhas
          */
-
     }
 }
