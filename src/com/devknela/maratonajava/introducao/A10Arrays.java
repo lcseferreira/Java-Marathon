f4a598ba10d8910deadcaaf581fa66eb50252d24
Lucas <lcseferreira@hotmail.com>
2022-05-10 23:07:40 -0300

    Outro modo de inicializar arrays

---
+++ src/com/devknela/maratonajava/introducao/A10Arrays.java
@@ -0,0 +1,10 @@
+package com.devknela.maratonajava.introducao;
+
+public class A10Arrays {
+    public static void main(String[] args) {
+        // Outro jeito de inicializar arrays
+        String[] nomes = new String[]{
+                "Lucas", "Eduardo", "Renata", "Lara"
+        };
+    }
+}
