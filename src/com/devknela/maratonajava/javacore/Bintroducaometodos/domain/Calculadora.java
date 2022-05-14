c626f0be2cf1129e18bf25eda83241d1eb27151f
Lucas <lcseferreira@hotmail.com>
2022-05-14 18:52:02 -0300

    POO: métodos

---
+++ src/com/devknela/maratonajava/javacore/Bintroducaometodos/domain/Calculadora.java
@@ -0,0 +1,23 @@
+package com.devknela.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class Calculadora {
+
+    // estrutura: modificadorDeAcesso retorno nomeDoMétodo(parâmetros) {o que ele faz}
+    public int somaDoisNumeros(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    public int subtraiDoisNumeros(int numero1, int numero2) {
+        return numero1 - numero2;
+    }
+
+    public int multiplicaDoisNumeros(int numero1, int numero2) {
+        return numero1 * numero2;
+    }
+
+    public int divideDoisNumeros(int numero1, int numero2) {
+        return numero1 / numero2;
+    }
+
+
+}
---
+++ src/com/devknela/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,11 @@
+package com.devknela.maratonajava.javacore.Bintroducaometodos.test;
+
+import com.devknela.maratonajava.javacore.Bintroducaometodos.domain.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        System.out.println(calculadora.somaDoisNumeros(1, 6));
+    }
+}
